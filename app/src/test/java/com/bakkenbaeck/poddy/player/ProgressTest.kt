9ccbc5dc414d5e5ec46b7a95791e1e0a76adf3f5
Martin <martin@bakkenbaeck.no>
2020-04-20 15:27:01 +0200

    Remove temp code

--- app/src/test/java/com/bakkenbaeck/poddy/player/ProgressTest.kt
+++ app/src/test/java/com/bakkenbaeck/poddy/player/ProgressTest.kt
@@ -1,19 +1,3 @@
 package com.bakkenbaeck.poddy.player
 
-import com.bakkenbaeck.poddy.PlayerHandler
-import com.bakkenbaeck.poddy.repository.QueueRepository
-
-class ProgressTest {
-
-    val queueRepository = QueueRepository(
-
-    )
-
-    val playerHandler = PlayerHandler(
-
-    )
-
-    fun testProgress() {
-
-    }
-}
+class ProgressTest {}
